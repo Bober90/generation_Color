74094e82eebbc95421589cdcd7d2ee956eb129cc
Trach Volodymyr <vtlviv90@gmail.com>
2023-03-29 15:33:26 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const block=document.querySelector('.js-block'),
+color=document.querySelector('.js-color'),
+btn=document.querySelector('.js-btn'),
+
+
+hex='0123456789ABCDEF';
+
+let randomColor='#';
+
+const getRandomColor=()=>{
+    randomColor='#';
+   // console.log(Math.random());
+    //console.log(Math.floor(Math.random()*hex.length));
+    //[0,1]
+
+    for (let i=0; i<6; i++){
+        randomColor=randomColor+hex[Math.floor(Math.random()*hex.length)];
+       
+    }
+    console.log(randomColor);
+};
+
+btn.addEventListener('click', ()=>{
+    getRandomColor();
+    block.style.backgroundColor=randomColor;
+    color.innerHTML=randomColor;
+})
+
